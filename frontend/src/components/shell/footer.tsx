bc4f6863d7553e4688c370765e9aeecfb7cb2354
limeal <paul.gazeau-rousseau@epitech.eu>
2024-08-29 11:22:25 +0200

    chore: patch footer

--- frontend/src/components/shell/footer.tsx
+++ frontend/src/components/shell/footer.tsx
@@ -1,6 +1,6 @@
 export default async function Footer() {
   return (
-    <footer className="absolute bottom-0 w-full p-4 text-center text-gray-500">
+    <footer className="w-full p-4 text-center text-gray-500">
       Made with ❤️ by <a href="https://github.com/limeal">Limeal</a>
     </footer>
   );
